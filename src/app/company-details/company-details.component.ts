50cfbca206c0755bb955de358d19a00865d3f087
ketakikk <kadamketaki25@gmail.com>
2020-09-14 15:19:15 +0530

    changes

--- src/app/company-details/company-details.component.ts
+++ src/app/company-details/company-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import * as SampleJson from '../../assets/json/company.json';
+
 
 @Component({
   selector: 'app-company-details',
@@ -11,6 +13,8 @@ export class CompanyDetailsComponent implements OnInit {
   public companyForm: FormGroup;
   copydata: any;
   constructor(private fb: FormBuilder, private http: HttpClient) {
+    console.log(SampleJson);
+
     this.companyForm = this.fb.group({
       comp_name: '',
       address: '',
@@ -26,22 +30,33 @@ export class CompanyDetailsComponent implements OnInit {
   }
   public jsonData: any;
   ngOnInit() {
-    this.http.get('/assets/json/company.json').subscribe((data: any) => {
-      console.log('com data', data[0]);
-      this.copydata = Object.assign({}, data[0]);
-      console.log('cpy data', this.copydata);
-      this.companyForm.controls["comp_name"].setValue(this.copydata.comp_name);
-      this.companyForm.controls["address"].setValue(this.copydata.address);
-      this.companyForm.controls["phone"].setValue(this.copydata.phone);
-      this.companyForm.controls["website"].setValue(this.copydata.website);
-      this.companyForm.controls["linkdin_url"].setValue(this.copydata.linkdin_url);
-      this.companyForm.controls["parent_copm"].setValue(this.copydata.parent_copm);
-      this.companyForm.controls["indusrty"].setValue(this.copydata.indusrty);
-      this.companyForm.controls["emp_size"].setValue(this.copydata.emp_size);
-      this.companyForm.controls["all_industry"].setValue(this.copydata.all_industry);
-      this.companyForm.controls["Revenue"].setValue(this.copydata.Revenue);
+    // this.http.get('/assets/json/company.json').subscribe((data: any) => {
+    //   console.log('com data', data[0]);
+    //   this.copydata = Object.assign({}, data[0]);
+    //   console.log('cpy data', this.copydata);
+    //   this.companyForm.controls["comp_name"].setValue(this.copydata.comp_name);
+    //   this.companyForm.controls["address"].setValue(this.copydata.address);
+    //   this.companyForm.controls["phone"].setValue(this.copydata.phone);
+    //   this.companyForm.controls["website"].setValue(this.copydata.website);
+    //   this.companyForm.controls["linkdin_url"].setValue(this.copydata.linkdin_url);
+    //   this.companyForm.controls["parent_copm"].setValue(this.copydata.parent_copm);
+    //   this.companyForm.controls["indusrty"].setValue(this.copydata.indusrty);
+    //   this.companyForm.controls["emp_size"].setValue(this.copydata.emp_size);
+    //   this.companyForm.controls["all_industry"].setValue(this.copydata.all_industry);
+    //   this.companyForm.controls["Revenue"].setValue(this.copydata.Revenue);
 
-    });
+    // });
+// console.log("SampleJson---> " ,SampleJson[0].Revenue)
+    this.companyForm.controls["comp_name"].setValue(SampleJson[0].comp_name);
+      this.companyForm.controls["address"].setValue(SampleJson[0].address);
+      this.companyForm.controls["phone"].setValue(SampleJson[0].phone);
+      this.companyForm.controls["website"].setValue(SampleJson[0].website);
+      this.companyForm.controls["linkdin_url"].setValue(SampleJson[0].linkdin_url);
+      this.companyForm.controls["parent_copm"].setValue(SampleJson[0].parent_copm);
+      this.companyForm.controls["indusrty"].setValue(SampleJson[0].indusrty);
+      this.companyForm.controls["emp_size"].setValue(SampleJson[0].emp_size);
+      this.companyForm.controls["all_industry"].setValue(SampleJson[0].all_industry);
+      this.companyForm.controls["Revenue"].setValue(SampleJson[0].Revenue);
 
   }
 
--- src/app/emp-list/emp-list.component.ts
+++ src/app/emp-list/emp-list.component.ts
@@ -3,14 +3,12 @@ import { FormGroup, FormBuilder, FormArray } from "@angular/forms";
 import { HttpClient } from "@angular/common/http";
 import { NgxSpinnerService } from "ngx-spinner";
 
-declare var $;
 
 @Component({
   selector: "app-emp-list",
   templateUrl: "./emp-list.component.html",
   styleUrls: ["./emp-list.component.css"],
 })
-
 export class EmpListComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
@@ -19,7 +17,10 @@ export class EmpListComponent implements OnInit {
   ) {}
   copydata: any = [];
   storedata: any = [];
-  searchText: any = '';
+  firstdata: any = [];
+  alldata: any = [];
+  firstfixdata:any=[];
+  searchText: any = "";
   datalength = 6;
   total: any;
   open(member) {
@@ -28,49 +29,88 @@ export class EmpListComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.spinner.show();
     this.getMembers();
   }
 
-
-  @HostListener('window:scroll', [])
-  onScroll(): void {
-    var bottom_position = $(document).height() - ($(window).scrollTop() + $(window).height());
-    // console.log("bott pos:",bottom_position, ",scroll:", $(window).scrollTop())
-    if (bottom_position <= 1500 && this.storedata !== undefined) {
-      if (this.datalength + 6 <= this.storedata.length) {
-        this.datalength += 6;
-      } else {
-        this.datalength = this.storedata.length;
-      }
-    }
-  }
-
+  // @HostListener("window:scroll", [])
+  // onScroll(): void {
+  //   var bottom_position =
+  //     $(document).height() - ($(window).scrollTop() + $(window).height());
+  //   // console.log("bott pos:",bottom_position, ",scroll:", $(window).scrollTop())
+  //   if (bottom_position <= 1500 && this.storedata !== undefined) {
+  //     if (this.datalength + 6 <= this.storedata.length) {
+  //       this.datalength += 6;
+  //     } else {
+  //       this.datalength = this.storedata.length;
+  //     }
+  //   }
+  // }
   getMembers() {
+     this.spinner.show();
     this.http.get("/assets/json/employee-list.json").subscribe((data: any) => {
-      this.spinner.hide();
       console.log("com data", data.Employees);
-      this.copydata = data.Employees;
-      this.storedata = data.Employees;
-
+      this.alldata = data.Employees;
+      this.firstdata = data.Employees.slice(0, 9);
+      this.firstfixdata = data.Employees.slice(0, 9);
+      console.log(" this.storedata---", this.firstdata);
       this.total = data.Employees.length;
     });
   }
 
-  searchData(event) {
-    console.log("--------->", event.target.valuent);
-    if (event.target.value === "") {
-      this.copydata = this.storedata;
-    } else {
-      this.copydata = this.storedata.filter((mem) => {
-        return (
-          mem.jobTitle
-            .toLowerCase()
-            .includes(event.target.value.toLowerCase()) ||
-          mem.name.toLowerCase().includes(event.target.value.toLowerCase())
-        );
-      });
-      console.log("--- this.copydata------>", this.copydata);
+  onScroll() {
+    // console.log("fis " ,this.firstdata.length)
+    // console.log("all-->",this.alldata.length)
+    if (this.firstdata.length < this.alldata.length) {
+      let fixedlength = this.firstdata.length;
+      let lastlenght = this.alldata.length - this.firstdata.length;
+      if (lastlenght > this.firstfixdata.length){
+         for (let i = fixedlength; i <= fixedlength + 9; i++) {
+          this.firstdata.push(this.alldata[i]);
+        }
+      }else{
+        for (let i = fixedlength; i <= fixedlength +lastlenght-1 ; i++) {
+          this.firstdata.push(this.alldata[i]);
+          console.log("--->" , i)
+          this.spinner.hide();
+        }
+      }
     }
+  }
+
+
+
+  // if (this.firstdata.length < this.alldata.length) {
+  //   let fixedlength = this.firstdata.length;
+  //   let lastlenght = this.alldata.length - this.firstdata.length;
+  //   console.log("length ---> ", this.firstdata.length);
+  //   console.log("length ---> ", lastlenght);
+  //   console.log("length ---> ", lastlenght);
+  //   if (fixedlength < lastlenght) {
+  //     for (let i = fixedlength; i <= fixedlength + 9; i++) {
+  //       this.firstdata.push(this.alldata[i]);
+  //     }
+  //     // console.log("------------>",this.firstdata)
+  //   }else{
+  //      console.log("------------>",this.firstdata)
+  //     for (let i = lastlenght; i <= lastlenght + 9; i++) {
+  //       this.firstdata.push(this.alldata[i]);
+  //     }
+  //   }
+  searchData(event) {
+    console.log("--------->", event.target.value);
+      if (event.target.value === "") {
+        console.log("in")
+        this.firstdata = this.alldata;
+      } else {
+        this.firstdata = this.alldata.filter((mem) => {
+          return (
+            mem.jobTitle
+              .toLowerCase()
+              .includes(event.target.value.toLowerCase()) ||
+            mem.name.toLowerCase().includes(event.target.value.toLowerCase())
+          );
+        });
+        console.log("--- this.copydata------>", this.copydata);
+      }
   }
 }
